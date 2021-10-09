1b3ebfd2713f09b6af10fc4666cd4904f5b5daca
vikashkshatra <kshatraviaksh@gmail.com>
2021-10-10 00:43:35 +0530

    file

---
+++ morse.py
@@ -0,0 +1,42 @@
+
+
+from typing import Dict
+
+
+codes= morseAlphabet ={
+        "A" : ".-",
+        "B" : "-...",
+        "C" : "-.-.",
+        "D" : "-..",
+        "E" : ".",
+        "F" : "..-.",
+        "G" : "--.",
+        "H" : "....",
+        "I" : "..",
+        "J" : ".---",
+        "K" : "-.-",
+        "L" : ".-..",
+        "M" : "--",
+        "N" : "-.",
+        "O" : "---",
+        "P" : ".--.",
+        "Q" : "--.-",
+        "R" : ".-.",
+        "S" : "...",
+        "T" : "-",
+        "U" : "..-",
+        "V" : "...-",
+        "W" : ".--",
+        "X" : "-..-",
+        "Y" : "-.--",
+        "Z" : "--..",
+        " " : "/"
+        }
+
+
+a = input("Enter your text here for morse_codes:-\n")
+
+
+# read morse code nad compare ites all alphabet
+# and give its code
+
---
+++ readme.txt
@@ -0,0 +1,5 @@
+"""
+this will convert your words in mosre code.
+
+"""
+- read morse code from file matches to your string and spits out mosre code:
