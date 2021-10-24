9a11936211ae1cff7b937e20ba972dea4e3e8f6e
vikashkshatra <kshatraviaksh@gmail.com>
2021-10-24 19:50:55 +0530

    recheck remaining

--- morse.py
+++ morse.py
@@ -1,66 +1,57 @@
 
+from playsound import playsound
+import time
 
-from typing import Dict
-
-
-codes={
-        "A" : ".-",
-        "B" : "-...",
-        "C" : "-.-.",
-        "D" : "-..",
-        "E" : ".",
-        "F" : "..-.",
-        "G" : "--.",
-        "H" : "....",
-        "I" : "..",
-        "J" : ".---",
-        "K" : "-.-",
-        "L" : ".-..",
-        "M" : "--",
-        "N" : "-.",
-        "O" : "---",
-        "P" : ".--.",
-        "Q" : "--.-",
-        "R" : ".-.",
-        "S" : "...",
-        "T" : "-",
-        "U" : "..-",
-        "V" : "...-",
-        "W" : ".--",
-        "X" : "-..-",
-        "Y" : "-.--",
-        "Z" : "--..",
-        " " : "/"
-        }
-
-
-a = input("Enter your phrase: \n")
-
-
+codes = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    " ": "/"
+}
+phrase = input("Enter your phrase: \n")
 
 # convert a string to morse code
-phrase= ""
-for word in a:
-        for i,j in zip(codes.keys(),codes.values()):
-                if word.upper() == i:
-                        phrase= phrase + j
-                        print(i)
-                        print(j)
-                        break
+morse_code = ""
+for word in phrase:
+    for i, j in zip(codes.keys(), codes.values()):
+        if word.upper() == i:
+            morse_code = morse_code + j
+            break
 
+print(morse_code)
+print("PLAYING SOUND")
 
-print(phrase)
-
-
-# code for reverse morse code search
-
-
-
-
-
-
-
-
-
-
-
+# play sound beeep
+for word in phrase:
+    for i, j in zip(codes.keys(), codes.values()):
+            dots = 0
+            if word.upper() == i :
+                for beep in j:
+                        if beep == ".":
+                                dots += 1
+                                playsound("beep.wav")
+                        else:
+                                time.sleep(0.2)
--- readme.txt
+++ readme.txt
@@ -3,3 +3,4 @@ this will convert your words in mosre code.
 
 """
 - read morse code from file matches to your string and spits out mosre code:
+- also plays sound :)
